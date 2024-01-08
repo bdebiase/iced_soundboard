fdd71f1afecd15368843bc47f78556d6d37dd543
ben <bdebiase2@gmail.com>
2024-01-08 18:33:33 -0500

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -10,25 +10,6 @@ use iced::{window, Application, Font, Settings};
 use style::{FONT_NAME, FONT_SIZE_DEFAULT};
 
 fn main() -> iced::Result {
-    // let (client, _status) =
-    //     jack::Client::new("rust_jack", jack::ClientOptions::NO_START_SERVER).unwrap();
-    // let out_port = client
-    //     .register_port("rust_out", jack::AudioOut::default())
-    //     .unwrap();
-
-    // let active_client = client.activate_async((), ()).unwrap();
-
-    // let ports = active_client
-    //     .as_client()
-    //     .ports(None, None, jack::PortFlags::empty());
-
-    // if let Some(first_port) = ports.get(0) {
-    //     active_client
-    //         .as_client()
-    //         .connect_ports_by_name(&out_port.name().unwrap(), &first_port)
-    //         .unwrap();
-    // }
-
     SoundboardApp::run(Settings {
         window: window::Settings {
             size: (500, 800),
